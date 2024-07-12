ded447fdc3942e00c87dd73f5753c90d81fe2f45
Andrii Kudenko <andrii.kudenko@gmail.com>
2024-07-12 19:44:56 -0400

    Implement orders management functionality for admin

--- src/components/cards/ShowPaymentInfo.js
+++ src/components/cards/ShowPaymentInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ShowPaymentInfo = ({order}) => (
+const ShowPaymentInfo = ({order, showStatus = true}) => (
     <div>
         <p>
             <span>Order Id: {order.paymentIntent.id}</span>{" / "}
@@ -11,7 +11,9 @@ const ShowPaymentInfo = ({order}) => (
             <span>Method: {order.paymentIntent.payment_method_types[0]}</span>{" / "}
             <span>Payment: {order.paymentIntent.status.toUpperCase()}</span>{" / "}
             <span>Ordered on: {new Date(order.paymentIntent.created * 1000).toLocaleString()}</span>{" / "}
-            <span className="badge bg-primary text-white">STATUS: {order.orderStatus}</span>
+            <br/>
+            {showStatus && <span className="badge bg-primary text-white">STATUS: {order.orderStatus}</span>}
+            
         </p>
     </div>
 )
